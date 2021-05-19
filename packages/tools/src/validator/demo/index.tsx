9827a6177b8324e08ca9ceb2282913f3f927259e
akihisahzy <1141754158@qq.com>
2021-05-19 23:15:21 +0800

    hzy

--- packages/tools/src/validator/demo/index.tsx
+++ packages/tools/src/validator/demo/index.tsx
@@ -1,7 +1,14 @@
+/*
+ * @Author: your name
+ * @Date: 2021-05-19 22:11:41
+ * @LastEditTime: 2021-05-19 23:15:06
+ * @LastEditors: Please set LastEditors
+ * @Description: In User Settings Edit
+ * @FilePath: \nicecode\packages\tools\src\validator\demo\index.tsx
+ */
 import React from 'react'
 import { validator } from '@nicecode/tools'
 
 export default () => {
   return <div>手机号正则：{String(validator('mobile'))}</div>;
 };
- 
